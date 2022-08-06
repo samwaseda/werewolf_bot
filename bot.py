c02eb642d7670695ec2d3d649168619f6e60e2a2
Sam Waseda <waseda@mpie.de>
2022-08-06 06:59:37 +0200

    current state

--- bot.py
+++ bot.py
@@ -28,42 +28,26 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class Distraction:
+class Master:
     def __init__(self):
-        self.current_word = None
-        with open('words.txt', 'r') as f:
-            self.all_words = f.read().split('\n')
+        self.werewolf_dict = {}
 
-    @staticmethod
-    def similarity(a, b):
-        return SequenceMatcher(None, a, b).ratio()
+    def create_key(self, update: Update, context: CallbackContext) -> str:
+        logger.info("%s created new group", update.message.from_user.first_name)
+        update.message.reply_text('How many people are participating?')
+        return 'get_number_of_players'
 
+    def get_number_of_players(self, update: Update, context: CallbackContext) -> str:
+        logger.info("%s chose %s players", update.message.from_user.first_name, update.message.text)
+        number_of_players = int(update.message.text)
+        update.message.reply_text('How many people are participating?')
+        return 'get_number_of_players'
 
-    def ask_question(self, update: Update, context: CallbackContext) -> int:
-        logger.info("%s set users %s", update.message.from_user.first_name, update.message.text)
-        self.current_word = np.random.choice(self.all_words)
-        values = np.array([self.similarity(sorted(self.current_word), sorted(w)) for w in self.all_words])
-        candidate_words = np.random.permutation(
-            np.array(words)[np.argsort(values)[-10:]]
-        ).reshape(-1, 2).tolist()
-        cw_random = ''.join(np.random.permutation(list(current_word)))
-        update.message.reply_text(
-            'What is the original word of: ' + cw_random,
-            reply_markup=ReplyKeyboardMarkup(
-                all_options,
-                one_time_keyboard=True,
-                resize_keyboard=True,
-            ),
-        )
-        return 'answer'
+def cancel(self, update: Update, context: CallbackContext) -> int:
+    logger.info("%s canceled", update.message.from_user.first_name)
+    return ConversationHandler.END
 
-    def answer(self, update: Update, context: CallbackContext) -> str:
-        logger.info("%s set users %s", update.message.from_user.first_name, update.message.text)
-        if self.similarity(self.current_word, update.message.text) == 1:
-            update.message.reply_text('Congratulations!')
-        return 'question'
-
-distraction = Distraction()
+master = Master()
 
 
 def main() -> None:
@@ -76,11 +60,10 @@ def main() -> None:
     text_non_command = Filters.text & ~Filters.command
     conv_handler = ConversationHandler(
         entry_points=[
-            CommandHandler('start', distraction.ask_question),
+            CommandHandler('start', master.create_key),
         ],
         states={
-            'question': [MessageHandler(text_non_command, distraction.ask_question)],
-            'answer': [MessageHandler(text_non_command, distraction.answer)],
+            'get_number_of_players': [MessageHandler(text_non_command, master.get_number_of_players)],
         },
         fallbacks=[
             CommandHandler('cancel', cancel),
